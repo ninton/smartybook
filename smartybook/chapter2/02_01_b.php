19e005be7b3f63e6b701f97abe9103099cf73abc
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-10-26 23:28:42 +0900

    CDROMの02_01_b.phpの内容が正しくない #6

--- smartybook/chapter2/02_01_b.php
+++ smartybook/chapter2/02_01_b.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>文字や数字を表示</title>
-</head>
-<body>
 <?php
 // ウェブサイト名
 $siteName = 'Smarty for Designers';
@@ -40,6 +33,3 @@ echo $category[0].'<br />';
 echo $category[1].'<br />';
 echo $category[2].'<br />';
 */
-?>
-</body>
-</html>
