d4c6c4b815064bd69119663a7339ea6faec86613
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-10-29 13:45:47 +0900

    PEAR JSONをjson_decodeに置き換え #14

--- xampp-linux/pear_download.sh
+++ xampp-linux/pear_download.sh
@@ -1,41 +1,41 @@
 #!/bin/bash -uex
-#
-#mkdir -p pear/auth
-#cd pear/auth
-#wget http://download.pear.php.net/package/Auth-1.6.4.tgz
-#tar xzf Auth-1.6.4.tgz
-#cd ../..
-#
-#
-#mkdir -p pear/net
-#cd pear/net
-#wget http://download.pear.php.net/package/Net_UserAgent_Mobile-1.0.0.tgz
-#tar xzf Net_UserAgent_Mobile-1.0.0.tgz
-#cd ../..
-#
-#mkdir -p pear/pager
-#cd pear/pager
-#wget http://download.pear.php.net/package/Pager-2.5.1.tgz
-#tar xzf Pager-2.5.1.tgz
-#cd ../..
-#
+
+mkdir -p pear/auth
+cd pear/auth
+wget http://download.pear.php.net/package/Auth-1.6.4.tgz
+tar xzf Auth-1.6.4.tgz
+cd ../..
+
+
+mkdir -p pear/net
+cd pear/net
+wget http://download.pear.php.net/package/Net_UserAgent_Mobile-1.0.0.tgz
+tar xzf Net_UserAgent_Mobile-1.0.0.tgz
+cd ../..
+
+mkdir -p pear/pager
+cd pear/pager
+wget http://download.pear.php.net/package/Pager-2.5.1.tgz
+tar xzf Pager-2.5.1.tgz
+cd ../..
+
 #mkdir -p pear/json
 #cd pear/json
 #wget http://download.pear.php.net/package/Services_JSON-1.0.3.tgz
 #tar xzf Services_JSON-1.0.3.tgz
 #cd ../..
-#
-#mkdir -p pear/amazon
-#cd pear/amazon
-#wget http://download.pear.php.net/package/Services_Amazon-0.9.0.tgz
-#tar xzf Services_Amazon-0.9.0.tgz
-#cd ../..
-#
-#mkdir -p pear/xml
-#cd pear/xml
-#wget http://download.pear.php.net/package/XML_Serializer-0.21.0.tgz
-#tar xzf XML_Serializer-0.21.0.tgz
-#cd ../..
+
+mkdir -p pear/amazon
+cd pear/amazon
+wget http://download.pear.php.net/package/Services_Amazon-0.9.0.tgz
+tar xzf Services_Amazon-0.9.0.tgz
+cd ../..
+
+mkdir -p pear/xml
+cd pear/xml
+wget http://download.pear.php.net/package/XML_Serializer-0.21.0.tgz
+tar xzf XML_Serializer-0.21.0.tgz
+cd ../..
 
 
 
@@ -49,7 +49,7 @@ cp -r  pear/net/Net_UserAgent_Mobile-1.0.0/Net $dst_dir
 cp     pear/pager/Pager-2.5.1/Pager.php $dst_dir
 cp -r  pear/pager/Pager-2.5.1/Pager     $dst_dir
 
-cp     pear/json/Services_JSON-1.0.3/JSON.php ../smartybook/chapter5_4/
+#cp     pear/json/Services_JSON-1.0.3/JSON.php ../smartybook/chapter5_4/
 
 
 cp  -r  pear/amazon/Services_Amazon-0.9.0/Services $dst_dir
