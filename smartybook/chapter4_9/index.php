0c5792ade40261239ef12dffe541e361b2889704
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-11-10 18:34:44 +0900

    カスタムプラグインのパス設定

--- smartybook/chapter4_9/index.php
+++ smartybook/chapter4_9/index.php
@@ -3,4 +3,5 @@
 require_once('../vendor/autoload.php');
 
 $smarty = new Smarty();
+$smarty->plugins_dir[] = __DIR__;
 $smarty->display('index.tpl');
--- smartybook/chapter5_6/_read/classes/AppSmarty.class.php
+++ smartybook/chapter5_6/_read/classes/AppSmarty.class.php
@@ -10,5 +10,6 @@ class AppSmarty extends Smarty
         $this->template_dir = dirname(__FILE__) . '/../../_read/templates';
         $this->compile_dir  = dirname(__FILE__) . '/../../_temp/templates_c';
         $this->cache_dir    = dirname(__FILE__) . '/../../_temp/cahce';
+        $this->plugins_dir[] = dirname(__FILE__) . '/../../';
     }
 }
