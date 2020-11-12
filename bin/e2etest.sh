7de344a51201097c22c9dad29eada75d52e5e9f1
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-11-12 21:09:32 +0900

    Seleniumテストケースのアップロードファイルのパス修正

--- bin/e2etest.sh
+++ bin/e2etest.sh
@@ -7,6 +7,12 @@ if [ "$n" != "0" ]; then
   find smartybook -name "007.jpg" | xargs -l1 rm
 fi
 
+n=$(find smartybook -name "%%*.php" | wc --line)
+if [ "$n" != "0" ]; then
+  find smartybook -name "%%*.php" | xargs -l1 rm
+fi
+
+
 cd selenium
 ./main.sh
 cd ..
--- selenium/main.sh
+++ selenium/main.sh
@@ -1,16 +1,23 @@
-#!/bin/bash -u
+#!/bin/bash -ux
 
 #error_log="../xampp-linux/lampp/logs/error_log"
 error_log="../xampp-linux/lampp/logs/php_error_log"
 
 sudo truncate --size=0 $error_log
 
+script_dir=$(pwd)
+imgpath=$script_dir/fixtures/007.jpg
+
+cat smartybook.side \
+  | sed "s#/home/aoki/Pictures/smartybook/007.jpg#$imgpath#g" \
+  >smartybook.side.tmp
+
 npx selenium-side-runner \
   -c "browserName=chrome goog:chromeOptions.args=[--headless,--nogpu]" \
   --debug \
   --output-directory=../results \
   --output-format=junit \
-  smartybook.side
+  smartybook.side.tmp
 
 grep "] PHP " $error_log
 
