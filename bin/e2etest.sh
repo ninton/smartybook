b49a26b0941f054e1476804b95bc20f80233ca6c
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-11-12 18:45:30 +0900

    chapter5_3/imgsizecvt.phpのseleniumテストケース

--- bin/e2etest.sh
+++ bin/e2etest.sh
@@ -2,6 +2,11 @@
 
 ./bin/db_init.sh
 
+n=$(find smartybook -name "007.jpg" | wc --line)
+if [ "$n" != "0" ]; then
+  find smartybook -name "007.jpg" | xargs -l1 rm
+fi
+
 cd selenium
 ./main.sh
 cd ..
