ca1a282430651e7be93c299872ff2ff8d4ac1632
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-11-14 14:54:47 +0900

    require_once Pager.phpを廃止し、autoloadのPagerを使うようにした

--- smartybook/chapter5_3/ini.php
+++ smartybook/chapter5_3/ini.php
@@ -2,7 +2,6 @@
 
 // PHP Strict Standards:  Non-static method Net_UserAgent_Mobile::factory() should not be called statically
 error_reporting(error_reporting() & ~E_STRICT & ~E_DEPRECATED);
-require_once('Pager.php');
 require_once(dirname(__FILE__) . '/../vendor/autoload.php');
 require_once(dirname(__FILE__) . '/../chapter4_1/ini.php');
 $CFG['SRCIMG_DIR'] = '../chapter4_1/images/';
