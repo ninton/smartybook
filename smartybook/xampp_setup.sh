ba9aea3e94303417be7292eda64acdaf50beb964
aoki.makoto <aoki_makoto@ninton.co.jp>
2020-11-14 14:53:48 +0900

    Pagerをcomposerでインストールした

--- smartybook/xampp_setup.sh
+++ smartybook/xampp_setup.sh
@@ -3,9 +3,11 @@
 cd /opt/lampp/etc
 
 cat php.ini \
-  | sed -r "s/date.timezone=.*/date.timezone=Asia\/Tokyo/" \
+  | sed -r "s/date.timezone\s*=.*/date.timezone = Asia\/Tokyo/" \
   | sed -r "s/;mbstring.internal_encoding\s*=\s*.*/mbstring.internal_encoding = utf-8/" \
   | sed -r "s/error_reporting\s*=\s*.*/error_reporting = E_ALL/" \
+  | sed -r "s/;include_path\s*=.*/include_path =\./" \
+  | sed -r "s/include_path\s*=.*/include_path =\./" \
   >php.ini.tmp
 
 cp php.ini.tmp  php.ini
