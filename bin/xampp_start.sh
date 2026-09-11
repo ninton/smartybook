04a82b1461200ec2413fb28ff8d07d73714f5bc9
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-12 08:56:59 +0900

    chore: sudo削除しました

--- bin/xampp_start.sh
+++ bin/xampp_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-sudo docker exec centos7.smartybook.docker /opt/lampp/htdocs/smartybook/xampp_start.sh
+docker exec centos7.smartybook.docker /opt/lampp/htdocs/smartybook/xampp_start.sh
