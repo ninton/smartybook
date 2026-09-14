2471393e2b73d307a502817983641020dcaffe95
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-14 18:03:21 +0900

    chore: shellcheck対応

--- scripts/local/patch-pear.sh
+++ scripts/local/patch-pear.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -ux
 
-cd lib/pear-pear.php.net
+cd lib/pear-pear.php.net || exit 1
 
-find . -name "*.php" | xargs -l1 grep --with-filename -e '=\s*&\s*new'
+find . -name "*.php" -print0 | xargs -0 -l1 grep --with-filename -e '=\s*&\s*new'
 
-find . -name "*.php" | xargs -l1 sed -ri 's/=\s*\&\s*new/= new/'
+find . -name "*.php" -print0 | xargs -0 -l1 sed -ri 's/=\s*\&\s*new/= new/'
 
-find . -name "*.php" | xargs -l1 grep --with-filename -e '=\s*&\s*new'
+find . -name "*.php" -print0 | xargs -0 -l1 grep --with-filename -e '=\s*&\s*new'
 
 
 sed -ri 's/function fetchData\(\$user, \$pass\)/function fetchData\(\$user, \$pass, \$isChallengeResponse = false\)/' Auth/Auth/Container/Array.php
