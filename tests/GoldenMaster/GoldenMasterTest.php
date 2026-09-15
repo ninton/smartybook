edbcff12e3c6a63333342addb5290ce1bdb8281f
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-15 23:11:10 +0900

    test: chapter4_5 調整

--- tests/GoldenMaster/GoldenMasterTest.php
+++ tests/GoldenMaster/GoldenMasterTest.php
@@ -112,7 +112,7 @@ final class GoldenMasterTest extends TestCase
 
     private function sanitizeHtml(string $html): string
     {
-            // 日付と時刻を置換
+        // 日付と時刻を置換
         $html = preg_replace(
             '/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}/',
             'YYYY-MM-DD HH:MM:SS',
@@ -126,8 +126,17 @@ final class GoldenMasterTest extends TestCase
             $html
         );
 
-        // 年月日時分秒のセレクトボックスの値を置換
+        if (str_contains($html, '時間毎にヘッダーを変える')) {
+            // chapter4_5/index.php 昼画像・夜画像
+            $html = str_replace('title_night.gif', 'title_REPLACED.gif', $html);
+            $html = str_replace('title_day.gif', 'title_REPLACED.gif', $html);
+
+            // chapter4_5/index.php 現在xx時
+            $html = preg_replace('/現在\d{1,2}時/', '現在XX時', $html);
+        }
+
         if (str_contains($html, 'startDate[Year]')) {
+            // chapter4_7/index.php 年月日時分秒のセレクトボックスの値を置換
             $html = str_replace(' selected="selected"', '', $html);
             $html = preg_replace(
                 '/<option value="(\d{4})">(\d{4})<\/option>/',
