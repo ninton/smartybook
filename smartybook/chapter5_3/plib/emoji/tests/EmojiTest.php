1fa9b71937294d23307595718e9dc0cace4b92a1
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-15 13:17:59 +0900

    test: EmojiTest.php をsimpletestからPHPUnitへ置き換えました (#70)
    * test: simpletest を PHPUnitへ置き換えました
    
    * test: dataProvider化しました
    
    * ci: ciでテストを回します

---
+++ smartybook/chapter5_3/plib/emoji/tests/EmojiTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SmartyBook\chapter5_3\plib\emoji\tests;
+
+use SmartyBook\chapter5_3\plib\emoji\Emoji;
+use PHPUnit\Framework\TestCase;
+
+final class EmojiTest extends TestCase
+{
+    /**
+     * @dataProvider emojiProvider
+     */
+    public function test(Emoji $emoji, string $input, string $expected): void
+    {
+        static::assertEquals($expected, $emoji->convert($input));
+    }
+
+    public function emojiProvider(): array
+    {
+        $ezweb = Emoji::singleton('i_uni16', 'e_img_num');
+        $softbank = Emoji::singleton('i_uni16', 's_uni16');
+
+        return [
+            [$ezweb, '&#xE63F;', '<img localsrc="107" />'],
+            [$softbank, '&#xE63F;', '&#xE049;'],
+        ];
+    }
+}
