356f7ec131010de106996a2499936a5861dc6c34
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-18 07:51:00 +0900

    fix: Smarty 5 からsetLeftDelimiterメソッドで設定するようになった
    Access to private property Smarty\Smarty::$left_delimiter.

--- smartybook/chapter5_2/photogallery.php
+++ smartybook/chapter5_2/photogallery.php
@@ -11,9 +11,9 @@ use Smarty\Smarty;
 require_once("../../vendor/autoload.php");
 // Smartyオブジェクトの作成
 $smarty = new Smarty();
-// デリミタタグの変更
-$smarty->left_delimiter  = "{{";
-$smarty->right_delimiter = "}}";
+// デリミタタグの変更: Smarty 5 からメソッドで設定するようになった
+$smarty->setLeftDelimiter("{{");
+$smarty->setRightDelimiter("}}");
 
 // 画像ファイルパスを取得（配列）
 $images = glob("$imgDir*.{jpg,png,gif}", GLOB_BRACE);
