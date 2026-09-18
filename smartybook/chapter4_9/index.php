65994d99899c88a9305971b184946beac10d25db
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-18 23:12:32 +0900

    cleanup: 不要なので削除しました

--- smartybook/chapter4_9/index.php
+++ smartybook/chapter4_9/index.php
@@ -4,8 +4,4 @@ require_once('../../vendor/autoload.php');
 
 $smarty = new Smarty();
 
-$plugins_dir = $smarty->plugins_dir;
-$plugins_dir[] = __DIR__;
-$smarty->plugins_dir = $plugins_dir;
-
 $smarty->display('index.tpl');
