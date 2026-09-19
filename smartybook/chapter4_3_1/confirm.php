48fdc8cae67a6bfddb3cd98a9b5f6b22a926561e
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-19 23:35:30 +0900

    fix: Warning 解消しました
    Warning:  Undefined array key "imageFile"

--- smartybook/chapter4_3_1/confirm.php
+++ smartybook/chapter4_3_1/confirm.php
@@ -13,6 +13,8 @@ if (!is_dir($imageDir)) {
 if (isset($_FILES["image"]["tmp_name"]) && is_uploaded_file($_FILES["image"]["tmp_name"])) {
     copy($_FILES["image"]["tmp_name"], $imageDir . $_FILES["image"]["name"]);
     $smarty->assign("imageFile", $imageDir . $_FILES["image"]["name"]);
+} else {
+    $smarty->assign("imageFile", "");
 }
 $smarty->assign("category", $_POST["category"]);
 $smarty->assign("title", stripslashes($_POST["title"]));
