796039fc930151538cf5a175f01b71c83d929e8b
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-19 14:50:12 +0900

    refactor: Smarty5準備: chapter5_4: insertタグ廃止予定、registerPluginで置き換えました

--- smartybook/chapter5_4/contents.php
+++ smartybook/chapter5_4/contents.php
@@ -4,6 +4,10 @@ require_once("ini.php");
 require_once("../../vendor/autoload.php");
 
 $smarty = new Smarty();
+
+require_once(__DIR__ . '/insert.php');
+$smarty->registerPlugin('function', 'insert_noticeText2', smarty_insert_noticeText2(...));
+
 $smarty->assign("siteName", $siteName);
 $smarty->assign("home", $home);
 $smarty->assign("categories", $categories);
