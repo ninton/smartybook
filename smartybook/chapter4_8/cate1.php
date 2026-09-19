0339338bb6cc17f2e00688704caf3b1ca2061ee4
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-19 14:08:21 +0900

    refactor: Smarty5準備: chapter4_8: preg_matchを暗黙で使うことができないので、registerPluginで登録します (#158)
    refactor: Smarty5準備: preg_matchを暗黙で使うことができないので、registerPluginで登録します

--- smartybook/chapter4_8/cate1.php
+++ smartybook/chapter4_8/cate1.php
@@ -2,6 +2,10 @@
 
 require_once('../../vendor/autoload.php');
 $smarty = new Smarty();
+
+// Smarty5準備: preg_match修飾子を登録
+$smarty->registerPlugin('modifier', 'preg_match', preg_match(...));
+
 $php = basename($_SERVER['SCRIPT_NAME']);
 $tpl = preg_replace('/\.php$/', '.tpl', $php);
 $smarty->display($tpl);
