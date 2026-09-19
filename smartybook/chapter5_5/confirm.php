42bd7a8cddd3dfe81d1c167b87ebf56d219c5559
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-19 23:30:21 +0900

    fix: warning undefined imageFile (#169)
    * fix: Warning 解消しました
    
    Warning:  Undefined array key "imageFile"
    
    * test: update Golden Master data

--- smartybook/chapter5_5/confirm.php
+++ smartybook/chapter5_5/confirm.php
@@ -20,6 +20,8 @@ if ($oAuth->getAuth()) {
         if (isset($_FILES["image"]["tmp_name"]) && is_uploaded_file($_FILES["image"]["tmp_name"])) {
             copy($_FILES["image"]["tmp_name"], $imageDir . $_FILES["image"]["name"]);
             $smarty->assign("imageFile", $imageDir . $_FILES["image"]["name"]);
+        } else {
+            $smarty->assign("imageFile", "");
         }
         $smarty->assign("category", $_POST["category"]);
         $smarty->assign("title", stripslashes($_POST["title"]));
