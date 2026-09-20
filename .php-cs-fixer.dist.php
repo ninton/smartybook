4de21e4419a72ec7c714e0037ef1c16a4a3c23c3
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-20 15:39:59 +0900

    ci: friendsofphp/php-cs-fixer (#181)
    * Install friendsofphp/php-cs-fixer
    
    * Add PHP CS Fixer configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,22 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__ . '/smartybook',
+        __DIR__ . '/tests',
+    ])
+    ->exclude(['templates_c', 'vendor']);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arrays', 'arguments', 'parameters'],
+        ],
+    ])
+    ->setFinder($finder)
+    ->setRiskyAllowed(false);
