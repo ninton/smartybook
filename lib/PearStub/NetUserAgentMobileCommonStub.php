2e8e72d978edfffb88813300c27b1e6f55841cef
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-25 18:47:13 +0900

    refactor: chapter5_3/src/ のスタブを lib/PearStub/ へ移動しました

--- lib/PearStub/NetUserAgentMobileCommonStub.php
+++ lib/PearStub/NetUserAgentMobileCommonStub.php
@@ -2,17 +2,22 @@
 
 declare(strict_types=1);
 
-namespace SmartyBook\chapter5_3\src;
+namespace Lib\PearStub;
 
 /**
  * 各キャリアのガラケーはサービス停止している
  * lib/の PEAR の Net_UserAgent_Mobile はもはやメンテナンスする意義が薄い
  * そのため、表示情報も固定値を返すスタブとして実装している
  */
-final class NetUserAgentMobileDisplayStub
+final class NetUserAgentMobileCommonStub
 {
-    public function getWidth(): int
+    public function getCarrierLongName(): string
     {
-        return 120;
+        return 'imode';
+    }
+
+    public function getDisplay(): NetUserAgentMobileDisplayStub
+    {
+        return new NetUserAgentMobileDisplayStub();
     }
 }
--- lib/PearStub/NetUserAgentMobileDisplayStub.php
+++ lib/PearStub/NetUserAgentMobileDisplayStub.php
@@ -2,22 +2,17 @@
 
 declare(strict_types=1);
 
-namespace SmartyBook\chapter5_3\src;
+namespace Lib\PearStub;
 
 /**
  * 各キャリアのガラケーはサービス停止している
  * lib/の PEAR の Net_UserAgent_Mobile はもはやメンテナンスする意義が薄い
  * そのため、表示情報も固定値を返すスタブとして実装している
  */
-final class NetUserAgentMobileCommonStub
+final class NetUserAgentMobileDisplayStub
 {
-    public function getCarrierLongName(): string
+    public function getWidth(): int
     {
-        return 'imode';
-    }
-
-    public function getDisplay(): NetUserAgentMobileDisplayStub
-    {
-        return new NetUserAgentMobileDisplayStub();
+        return 120;
     }
 }
