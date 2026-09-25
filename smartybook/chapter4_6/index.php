df2e5d8d7215e8e7fb964c9f4339720742418f05
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-26 00:51:02 +0900

    refactor: Pagerクラスのスタブを用意して、置き換えました (#305)
    * refactor: Pagerクラスのスタブを用意して、置き換えました
    
    * Squashed commit of the following:
    
    commit 0638082280760c27b2b81f08a2db88ce60687971
    Author: Ninton G.K. <aoki_makoto@ninton.co.jp>
    Date:   Sat Sep 26 00:17:04 2026 +0900
    
        chore: PHPStan、php-cs-fixer対象に lib/PearStub/ を追加しました (#306)
    
    * style: format
    
    * refactor: PHPStan対応
    
    * fix: copilot指摘。送信パラメータの setPerPage も考慮します
    
    * fix: 不要コードを削除しました
    
    * docs: スタブです。引数を無視して、固定のHTMLを返します。
    
    * refactor: ページ番号の正規化処理の位置を移動しました
    
    * fix: copilot指摘。 `pageHtmlArr` が正しい

--- smartybook/chapter4_6/index.php
+++ smartybook/chapter4_6/index.php
@@ -3,7 +3,7 @@
 /**
  * @var array{dsn: string, db_user: string, db_password: string} $CONFIG
  */
-
+use Lib\PearStub\PagerStub as Pager;
 use Smarty\Smarty;
 
 require_once('../../vendor/autoload.php');
@@ -36,19 +36,21 @@ if (empty($_REQUEST['setPerPage'])) {
 // 全件数を調べて、Pagerを初期化する
 $cms = new CMS($CONFIG['dsn'], $CONFIG['db_user'], $CONFIG['db_password']);
 
-$params = [];
-$params['totalItems'] = $cms->getCount();
-$pager = Pager::factory($params);
-
 // ページ番号の調整
 if ((int)$_REQUEST['pageID'] < 1) {
     $_REQUEST['pageID'] = 1;
 }
-if ($pager->numPages() < $_REQUEST['pageID']) {
+if ($cms->getCount() < $_REQUEST['pageID']) {
     $_REQUEST['pageID'] = 1;
 }
+
+$params = [];
+$params['totalItems'] = $cms->getCount();
+$params['currentPage'] = (int)$_REQUEST['pageID'];
+$pager = Pager::factory($params);
+
 // ページに表示する範囲のデータを読みだす
-list($from, $to) = $pager->getOffsetByPageId($_REQUEST['pageID']);
+list($from, $to) = $pager->getOffsetByPageId();
 $rcd_arr = [];
 if ((0 < $from) && (0 < $to)) {
     $rcd_arr = $cms->getAll($from - 1, $to - $from + 1, $_REQUEST['sort'], $_REQUEST['order']);
