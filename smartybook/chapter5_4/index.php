1867edaa841d1b2e2e0f02977144cee6f3f30754
Ninton G.K. <aoki_makoto@ninton.co.jp>
2026-09-25 14:32:23 +0900

    chore: コメントにダミーデータJSONについて記載。Twitter URL設定は不要なので、削除しました (#300)
    * chore: コメントにダミーデータJSONについて記載。Twitter URL設定は不要なので、削除しました
    
    Twitter API はサービス停止。
    ダミーデータJSONを読むこむように修正済み。
    URL設定は不要なので、削除しました。
    コメントを修正しました
    
    * refactor: copilot指摘。__DIR__ を使うようにしました。

--- smartybook/chapter5_4/index.php
+++ smartybook/chapter5_4/index.php
@@ -55,9 +55,8 @@ if (!$smarty->isCached('index.tpl')) {
     $smarty->assign('data', $data);
     $smarty->assign('picture', $picture);
 
-    //Twitter APIからのコンテンツ読み込み
-    $twitterUrl =  'http://twitter.com/statuses/user_timeline/kara_d.json';
-    $twitterUrl =  'kara_d.json';
+    /** @note Twitter API はサービス停止しました。代わりにダミーデータJSONを読み込みます */
+    $twitterUrl =  __DIR__ . '/kara_d.json';
     $jTwitter = file_get_contents($twitterUrl);
     $aTwitter = json_decode($jTwitter);
     $smarty->assign('aTwitter', $aTwitter);
