4491739cc3b5b3a13de95b71ebc16c19533a1cc8
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-26 22:41:40 +0900

    refactor: DB情報を環境変数から取得するようにしました

--- smartybook/chapter4_6/config.php
+++ smartybook/chapter4_6/config.php
@@ -1,7 +1,26 @@
 <?php
 
-// PDO用
-// @fixme .env から読み込むようにしたい
-$CONFIG['dsn'] = 'mysql:dbname=smartybook;host=db';
-$CONFIG['db_user'] = 'user';
-$CONFIG['db_password'] = 'password';
+declare(strict_types=1);
+
+/**
+ * 環境変数を文字列として安全に取得する
+ */
+function envString(string $key, string $defaultValue = ''): string
+{
+    $val = $_ENV[$key] ?? getenv($key);
+    if (is_string($val) && $val !== '') {
+        return $val;
+    }
+    return $defaultValue;
+}
+
+// $_ENV または getenv から安全に取得（型を string に確定させる）
+$dbHost     = envString('DB_HOST');
+$dbDatabase = envString('DB_DATABASE');
+$dbUser     = envString('DB_USER');
+$dbPassword = envString('DB_PASSWORD');
+
+// PDO設定
+$CONFIG['dsn'] = sprintf('mysql:dbname=%s;host=%s', $dbDatabase, $dbHost);
+$CONFIG['db_user'] = $dbUser;
+$CONFIG['db_password'] = $dbPassword;
