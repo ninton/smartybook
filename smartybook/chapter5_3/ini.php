ab62094a3e039092b44cf20bc7ffd03db4ad2929
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-26 17:54:39 +0900

    fix: `$csv` には絶対パスが設定されている

--- smartybook/chapter5_3/ini.php
+++ smartybook/chapter5_3/ini.php
@@ -6,8 +6,9 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../chapter4_1/ini.php';
 
 /**
- * @var string $csv
+ * @fixme chapter5_3 配下にdata.csvを配置したい
+ * @var string $csv chapter4_1/ini.phpで絶対パス定義されている
  */
 $CFG['SRCIMG_DIR'] = '../chapter4_1/images/';
 $CFG['DSTIMG_DIR'] = './images/';
-$CFG['CSV_FILE'  ] = "../chapter4_1/$csv";
+$CFG['CSV_FILE'  ] = $csv;
