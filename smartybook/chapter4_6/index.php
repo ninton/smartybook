13476fd6472ad6584b6e0972c128be36998dc839
aoki.makoto <aoki_makoto@ninton.co.jp>
2026-09-26 22:39:30 +0900

    style: use文をまとめました

--- smartybook/chapter4_6/index.php
+++ smartybook/chapter4_6/index.php
@@ -5,12 +5,12 @@
  */
 use Lib\PearStub\PagerStub as Pager;
 use Smarty\Smarty;
+use SmartyBook\chapter4_6\CMS;
+use SmartyBook\chapter4_6\SortNavigator;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 
-use SmartyBook\chapter4_6\CMS;
-use SmartyBook\chapter4_6\SortNavigator;
 
 $smarty = new Smarty();
 $smarty->configLoad('index.conf');
